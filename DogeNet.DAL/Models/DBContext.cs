7c0dd4ac375c91f73cebae730124c4ba91437a35
Adrian Liu <aliu@leobit.com>
2021-10-26 18:37:38 +0300

    DBContextAdded

---
+++ DogeNet.DAL/Models/DBContext.cs
@@ -0,0 +1,314 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogeNet.DAL.Models
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<BlackList> BlackLists { get; set; }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Conversation> Conversations { get; set; }
+        public virtual DbSet<ConversationParticipant> ConversationParticipants { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Friend> Friends { get; set; }
+        public virtual DbSet<FriendRequest> FriendRequests { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<GroupParticipant> GroupParticipants { get; set; }
+        public virtual DbSet<Like> Likes { get; set; }
+        public virtual DbSet<Message> Messages { get; set; }
+        public virtual DbSet<OwnerPost> OwnerPosts { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<PostContent> PostContents { get; set; }
+        public virtual DbSet<Status> Statuses { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=DESKTOP-QV2VVS8\\MSSQLSERVER01;Database=DogeNetDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<BlackList>(entity =>
+            {
+                entity.ToTable("BlackList");
+
+                entity.Property(e => e.AddedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Friend)
+                    .WithMany(p => p.BlackListFriends)
+                    .HasForeignKey(d => d.FriendId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_BlackList_Users");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.BlackListUsers)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_BlackList_Users1");
+            });
+
+            modelBuilder.Entity<City>(entity =>
+            {
+                entity.HasOne(d => d.Country)
+                    .WithMany(p => p.Cities)
+                    .HasForeignKey(d => d.CountryId)
+                    .HasConstraintName("FK_Cities_Countries");
+            });
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.Property(e => e.AddedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.Text).IsRequired();
+
+                entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Post)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.PostId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Comments_Posts");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Comments_Users");
+            });
+
+            modelBuilder.Entity<Conversation>(entity =>
+            {
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.Title).IsRequired();
+
+                entity.HasOne(d => d.Creator)
+                    .WithMany(p => p.Conversations)
+                    .HasForeignKey(d => d.CreatorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Conversations_Users");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Conversations)
+                    .HasForeignKey(d => d.StatusId)
+                    .HasConstraintName("FK_Conversations_Statuses");
+            });
+
+            modelBuilder.Entity<ConversationParticipant>(entity =>
+            {
+                entity.Property(e => e.AddedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Conversation)
+                    .WithMany(p => p.ConversationParticipants)
+                    .HasForeignKey(d => d.ConversationId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ConversationParticipants_Conversations");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.ConversationParticipants)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ConversationParticipants_Users");
+            });
+
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Friend>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.FriendNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.FriendId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Friends_Users");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Friends_Users1");
+            });
+
+            modelBuilder.Entity<FriendRequest>(entity =>
+            {
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Friend)
+                    .WithMany(p => p.FriendRequestFriends)
+                    .HasForeignKey(d => d.FriendId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_FriendRequests_Users");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.FriendRequestUsers)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_FriendRequests_Users1");
+            });
+
+            modelBuilder.Entity<Group>(entity =>
+            {
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.Name).IsRequired();
+
+                entity.HasOne(d => d.Creator)
+                    .WithMany(p => p.Groups)
+                    .HasForeignKey(d => d.CreatorId)
+                    .HasConstraintName("FK_Groups_Users");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Groups)
+                    .HasForeignKey(d => d.StatusId)
+                    .HasConstraintName("FK_Groups_Statuses");
+            });
+
+            modelBuilder.Entity<GroupParticipant>(entity =>
+            {
+                entity.Property(e => e.AddedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Group)
+                    .WithMany(p => p.GroupParticipants)
+                    .HasForeignKey(d => d.GroupId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_GroupParticipants_Groups");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.GroupParticipants)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_GroupParticipants_Users");
+            });
+
+            modelBuilder.Entity<Like>(entity =>
+            {
+                entity.HasOne(d => d.Post)
+                    .WithMany(p => p.Likes)
+                    .HasForeignKey(d => d.PostId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Likes_Posts");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Likes)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Likes_Users");
+            });
+
+            modelBuilder.Entity<Message>(entity =>
+            {
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Conversation)
+                    .WithMany(p => p.Messages)
+                    .HasForeignKey(d => d.ConversationId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Messages_Conversations");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Messages)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Messages_Users");
+            });
+
+            modelBuilder.Entity<OwnerPost>(entity =>
+            {
+                entity.ToTable("OwnerPost");
+
+                entity.HasOne(d => d.Owner)
+                    .WithMany(p => p.OwnerPosts)
+                    .HasForeignKey(d => d.OwnerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OwnerPost_Users");
+
+                entity.HasOne(d => d.Post)
+                    .WithMany(p => p.OwnerPosts)
+                    .HasForeignKey(d => d.PostId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OwnerPost_Groups");
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Creator)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.CreatorId)
+                    .HasConstraintName("FK_Posts_Users");
+            });
+
+            modelBuilder.Entity<PostContent>(entity =>
+            {
+                entity.ToTable("PostContent");
+
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Post)
+                    .WithMany(p => p.PostContents)
+                    .HasForeignKey(d => d.PostId)
+                    .HasConstraintName("FK_PostContent_Posts");
+            });
+
+            modelBuilder.Entity<Status>(entity =>
+            {
+                entity.Property(e => e.StatusName).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.FirstName).IsRequired();
+
+                entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+                entity.HasOne(d => d.City)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.CityId)
+                    .HasConstraintName("FK_Users_Cities");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.StatusId)
+                    .HasConstraintName("FK_Users_Statuses");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
