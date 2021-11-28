919744cd260eacb06dd1b0f0e25c4b5eab99b780
Adrian Liu <aliu@leobit.com>
2021-11-28 22:59:26 +0200

    xUnit Tests

---
+++ DogeNet.Tests/BLL/Features/Account/CreateAccountMappingProfileTest.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="CreateAccountMappingProfileTest.cs" company="Leobit">
+// Copyright (c) Leobit. All rights reserved.
+// </copyright>
+
+namespace DogeNet.Tests.BLL.Features.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using DogeNet.BLL.Features.Account.CreateAccount;
+    using DogeNet.DAL.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Xunit;
+
+    public class CreateAccountMappingProfileTest
+    {
+        [Fact]
+        public void ToIdentityUserMap()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = "UserNameTest",
+                Password = "UserPassword123#",
+                RepeatPassword = "UserPassword123#",
+                FirstName = "UserFirstName",
+                LastName = "UserLastName",
+                Email = "test@Email.com",
+            };
+
+            var user = new IdentityUser();
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CreateAccountModelProfiles>());
+            var mapper = config.CreateMapper();
+
+            user = mapper.Map<CreateAccountModel, IdentityUser>(model);
+
+            Assert.True(user.UserName == model.UserName && user.Email == model.Email);
+        }
+
+        [Fact]
+        public void ToAppUserMap()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = "UserNameTest",
+                Password = "UserPassword123#",
+                RepeatPassword = "UserPassword123#",
+                FirstName = "UserFirstName",
+                LastName = "UserLastName",
+                Email = "test@Email.com",
+            };
+
+            var user = new User();
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CreateAccountModelProfiles>());
+            var mapper = config.CreateMapper();
+
+            user = mapper.Map<CreateAccountModel, User>(model);
+
+            Assert.True(user.UserName == model.UserName && user.FirstName == model.FirstName && user.LastName == model.LastName);
+        }
+    }
+}
---
+++ DogeNet.Tests/BLL/Features/Account/CreateAccountValidatorTests.cs
@@ -0,0 +1,150 @@
+﻿// <copyright file="CreateAccountValidatorTests.cs" company="Leobit">
+// Copyright (c) Leobit. All rights reserved.
+// </copyright>
+
+namespace DogeNet.Tests.BLL.Features.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DogeNet.BLL.Features.Account.CreateAccount;
+    using Xunit;
+
+    public class CreateAccountValidatorTests
+    {
+        [Fact]
+        public void ValidationNormal()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = "UserNameTest",
+                Password = "UserPassword123#",
+                RepeatPassword = "UserPassword123#",
+                FirstName = "UserFirstName",
+                LastName = "UserLastName",
+                Email = "test@Email.com",
+            };
+            CreateAccountValidator validator = new CreateAccountValidator();
+
+            var validationResults = validator.Validate(model);
+
+            Assert.True(validationResults.IsValid);
+        }
+
+        [Fact]
+        public void ValidationWrongEmailFormat()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = "UserNameTest",
+                Password = "UserPassword123#",
+                RepeatPassword = "UserPassword123#",
+                FirstName = "UserFirstName",
+                LastName = "UserLastName",
+                Email = "testEm#ail.com",
+            };
+            CreateAccountValidator validator = new CreateAccountValidator();
+
+            var validationResults = validator.Validate(model);
+
+            Assert.False(validationResults.IsValid);
+        }
+
+        [Fact]
+        public void ValidationPasswordsNotMatch()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = "UserNameTest",
+                Password = "UserPassword123#",
+                RepeatPassword = "UserPassword12efffewf3#",
+                FirstName = "UserFirstName",
+                LastName = "UserLastName",
+                Email = "test@Email.com",
+            };
+            CreateAccountValidator validator = new CreateAccountValidator();
+
+            var validationResults = validator.Validate(model);
+
+            Assert.False(validationResults.IsValid);
+        }
+
+        [Fact]
+        public void ValidationPasswordTooShort()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = "UserNameTest",
+                Password = "123",
+                RepeatPassword = "123",
+                FirstName = "UserFirstName",
+                LastName = "UserLastName",
+                Email = "test@Email.com",
+            };
+            CreateAccountValidator validator = new CreateAccountValidator();
+
+            var validationResults = validator.Validate(model);
+
+            Assert.False(validationResults.IsValid);
+        }
+
+        [Fact]
+        public void ValidationTooLongStrings()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = "UserNam434124442142412421442421455555555555555555555555555555555555544555555555555555555521412421421424124164354435533545eTest",
+                Password = "UserPassword12ef5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555ffewf3#",
+                RepeatPassword = "UserPassword12ef5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555ffewf3#",
+                FirstName = "UserNam434124442142412421442421455555555555555555555555555555555555544555555555555555555521412421421424124164354435533545eTest",
+                LastName = "UserNam434124442142412421442421455555555555555555555555555555555555544555555555555555555521412421421424124164354435533545eTest",
+                Email = "UserNam434124442142412421442421455555555555555555555555555555555555544555555555555555555521412421421424124164354435533545eTest@Email.com",
+            };
+            CreateAccountValidator validator = new CreateAccountValidator();
+
+            var validationResults = validator.Validate(model);
+
+            Assert.False(validationResults.IsValid);
+        }
+
+        [Fact]
+        public void ValidationEmptyStrings()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = string.Empty,
+                Password = string.Empty,
+                RepeatPassword = string.Empty,
+                FirstName = string.Empty,
+                LastName = string.Empty,
+                Email = string.Empty,
+            };
+            CreateAccountValidator validator = new CreateAccountValidator();
+
+            var validationResults = validator.Validate(model);
+
+            Assert.False(validationResults.IsValid);
+        }
+
+        [Fact]
+        public void ValidationNullStrings()
+        {
+            CreateAccountModel model = new CreateAccountModel()
+            {
+                UserName = null,
+                Password = null,
+                RepeatPassword = null,
+                FirstName = null,
+                LastName = null,
+                Email = null,
+            };
+            CreateAccountValidator validator = new CreateAccountValidator();
+
+            var validationResults = validator.Validate(model);
+
+            Assert.False(validationResults.IsValid);
+        }
+    }
+}
